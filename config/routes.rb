bb4f58aac17f0df5f3c9cbb951eb396ffe91eae4
Michael Anderson <michanders@msn.com>
2018-04-03 19:07:58 +0000

    Adduser authentication

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users, :mustangs
+  root "users#show"
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+end
